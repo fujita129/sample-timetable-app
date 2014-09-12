a97ffccee68907750c4ff81998eb1210438ac491
fujita129 <shunfujita1222@gmail.com>
2014-09-12 16:31:32 +0900

    修正

--- app/models/Koma.java
+++ app/models/Koma.java
@@ -8,8 +8,10 @@ import play.data.validation.*;
 public class Koma extends Model {
     private static final long serialVersionUID = 1L;
 
+    @Id
     public Long id;
 
+    @Constraints.Required
     public String name;
 
     public static Finder<Long,Koma> findKoma = new Finder<Long,Koma>(Long.class, Koma.class);
--- conf/evolutions/default/2.sql
+++
@@ -1,5 +0,0 @@
-# --- Sample dataset
-
-# --- !Ups
-
-insert into koma (id,name) values (  1,'English');
