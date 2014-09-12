f213a942250cc0715841c2e15aa2388b81dc08bd
fujita129 <shunfujita1222@gmail.com>
2014-09-12 16:57:09 +0900

    項目の追加

--- app/models/Koma.java
+++ app/models/Koma.java
@@ -14,6 +14,15 @@ public class Koma extends Model {
     @Constraints.Required
     public String name;
 
+    @Constraints.Required
+    public String day;
+
+    @Constraints.Required
+    public String num;
+
+    @Constraints.Required
+    public String prof;
+
     public static Finder<Long,Koma> findKoma = new Finder<Long,Koma>(Long.class, Koma.class);
 
 }
